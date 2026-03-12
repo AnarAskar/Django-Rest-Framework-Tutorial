d875a23f2342ebd6720db70054f7f307e8a32e19
AnarAskar <anaraskar356@gmail.com>
2026-03-12 03:32:41 +0300

    Created the first api View

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,11 +2,11 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # API -> Application programming interface
 print(get_response.text) # Raw text response
 print(get_response.status_code)
 
-# print(get_response.json())
+print(get_response.json()["message"])
 # print(get_response.status_code)
