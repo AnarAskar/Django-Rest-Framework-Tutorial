fb9033466c3974775b5da8dc65ce26632b544363
AnarAskar <anaraskar356@gmail.com>
2026-03-12 03:46:07 +0300

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,20 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django REST API response"})
+
+    print(request.GET)
+    print(request.POST)
+
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data.keys())
+    # data["headers"] = request.headers
+    data["params"] = dict(request.GET)
+    data["headers"] = json.dumps(dict(request.headers))
+    data["content_type"] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,8 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # API -> Application programming interface
-print(get_response.text) # Raw text response
-print(get_response.status_code)
+# print(get_response.text) # Raw text response
+# print(get_response.status_code)
 
-print(get_response.json()["message"])
+print(get_response.json())
 # print(get_response.status_code)
